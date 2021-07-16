9eb4a13f9926a7ddffb46a6bf362e47b54ecd9a0
AdityaAmbekar <02aditya96@gmail.com>
2021-07-16 14:12:34 +0530

    First draft

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+// const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookie-parser');
+const dotenv = require('dotenv')
+
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+// view engine
+app.set('view engine', 'ejs');
+
+// middleware
+app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser());
+
+// routes
+// app.get('*', checkUser);
+app.get('/', (req, res) => res.render('home'));
+// app.get('/grep', requireAuth, (req, res) => res.render('grep'));
+
+
+app.listen(port, () => {
+    console.log('server is running on port ' + port);
+})
